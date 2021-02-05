2ae517015d5d89031525acb26ecc0797cc139b0a
ThomasDelepine <ThomasDelepine@gmail.com>
2021-02-05 12:56:24 +0000

    <màj le 05/02> : copy() de open_digraph bug au niveau  de la copie du champs nodes

--- tests/open_digraph_test.py
+++ tests/open_digraph_test.py
@@ -30,8 +30,8 @@ class NodeTest(unittest.TestCase):
     self.n0 = node(0, 'i', [], [1])
     self.n1 = node(1, 'j', [0], [])
 
-  def copy_test(self):
-    n2 = self.n0.copy
+  def test_copyNode(self):
+    n2 = self.n0.copy()
     self.assertEqual(n2.label, self.n0.label)
     self.assertEqual(n2.id, self.n0.id)
     self.assertIsNot(n2, self.n0)
@@ -41,16 +41,31 @@ class GraphTest(unittest.TestCase):
   def setUp(self):
     self.n0 = node(0, 'i', [], [1])
     self.n1 = node(1, 'j', [0], [])
-    g = open_digraph([0], [1], [n0, n1])
+    self.g = open_digraph([0], [1], [self.n0, self.n1])
   
-  def copy_test(self):
+  def test_copyGraph(self):
     g2 = self.g.copy()
     self.assertEqual(g2.inputs ,self.g.inputs)
     self.assertEqual(g2.outputs ,self.g.outputs)
     self.assertEqual(g2.nodes ,self.g.nodes)
     self.assertIsNot(g2,self.g)
-  
 
+class gettersTest(unittest.TestCase):
+
+  def setUp(self):
+    self.n0 = node(0, 'i', [], [1])
+    self.n1 = node(1, 'j', [0], [])
+    self.g = open_digraph([0], [1], [self.n0, self.n1])
+
+  def test_nodeGetters(self):
+    self.assertEqual(self.n0.get_id(), 0)
+    self.assertEqual(self.n1.get_id(), 1)
+    self.assertEqual(self.n0.get_label(), 'i')
+    self.assertEqual(self.n1.get_label(), 'j')
+    self.assertEqual(self.n0.get_parents_ids(), [])
+    self.assertEqual(self.n1.get_parents_ids(), [0])
+    self.assertEqual(self.n0.get_parents_ids(), [])
+    self.assertEqual(self.n1.get_parents_ids(), [0])
 if __name__ == '__main__':  # the following code is called only when
   unittest.main()           # precisely this file is run.
 
