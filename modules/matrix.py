7960554c7229c4b0b1a83973cc7e914860985b46
ThomasDelepine <ThomasDelepine@gmail.com>
2021-02-13 17:23:40 +0000

    8 ok et testé

--- modules/matrix.py
+++ modules/matrix.py
@@ -1,33 +1,83 @@
 import random
+from modules.open_digraph import *
 
 def random_int_list(n, bound): #@param : n (int) : nb éléments | bound (int) : valeur max
   liste = []
   for i in range(0,n):
     liste.append(random.randint(0,bound))
   return liste #@return : int list
-####################################################
-def random_int_matrix(n, bound, null_diag=False):#@param : n (int) : nb éléments | bound (int) : valeur max | null_diag (boolean)
+
+
+def random_matrix(n, bound, null_diag=False, symetric=False, oriented=False, triangular=False):
   liste = []
   for element in range(0,n):
     liste.append(random_int_list(n,bound))
   if null_diag:
     for i in range(0,n):
       liste[i][i] = 0
-  return liste #@return : int list list
-####################################################
-def random_symetric_int_matrix(n, bound,null_diag=False):#@param : n (int) : nb éléments | bound (int) : valeur max | null_diag (boolean)
-  liste = []
-  for i in range(0, n):
-    liste.append([0 for j in range(0, n)])
-  for i in range(0, n):
-    for j in range(0, i + 1):
-      r = random.randint(0,bound)
-      liste[i][j] = liste[j][i] = r
-  if null_diag:
-    for i in range(0,n):
-      liste[i][i] = 0
-  return liste #@return : int list list
+  if symetric:
+    for i in range(0, n):
+      for j in range(0, i + 1):
+        liste[i][j] = liste[j][i]
+  if oriented:
+    for i in range(0, n):
+      for j in range(0, n):
+        if liste[i][j] != 0 and liste[j][i] != 0:
+          rand = random.randint(0, 1)
+          if rand == 0:
+            liste[i][j] = 0
+          else :
+            liste[j][i] = 0
+  if triangular:
+    for i in range(0, n):
+      for j in range(i + 1, n):
+        liste[i][j] = 0
 
+  return liste
+
+def  graph_from_adjacency_matrix(matrix):
+  nodeNumber = len(matrix)
+  nodeList = []
+  for i in range(nodeNumber):
+    nodeList.append(node(i, "value", [], []))
+  for i in range(nodeNumber):
+    for j in range(nodeNumber):
+      for iteration in range(matrix[i][j]):
+        nodeList[i].add_child_id(j)
+        nodeList[j].add_parent_id(i)
+  return open_digraph([],[],nodeList)
+
+def random_graph(n, bound, inputs=0, outputs=0, form="free"):
+  '''
+  form:
+    "free" = graph sans contraintes
+    "DAG" = graph dirigé acyclique
+    "oriented" = graph dirigé avec arête entre 2 nodes que dans un sens
+    "undirected" = graph non-dirigé : symétrique
+    "loop-free undirected" = sans boucle et non-dirigé
+
+  '''
+  if form=="free":
+    g = graph_from_adjacency_matrix(random_matrix(n, bound))
+  elif form=="DAG":
+    g = graph_from_adjacency_matrix(random_matrix(n, bound, null_diag=True, triangular=True))
+  elif form=="oriented":
+    g = graph_from_adjacency_matrix(random_matrix(n, bound, null_diag=True, oriented=True))
+  elif form=="undirected":
+    g = graph_from_adjacency_matrix(random_matrix(n, bound, symetric=True))
+  elif form=="loop-free undirected":
+    g = graph_from_adjacency_matrix(random_matrix(n, bound, null_diag=True, symetric=True))
+  l = list(range(n))
+  for i in range(inputs):
+    e = random.choice(l)
+    g.add_input_id(e)
+    l.remove(e)
+  l = list(range(n))
+  for i in range(outputs):
+    e = random.choice(l)
+    g.add_output_id(e)
+    l.remove(e)
+  return g
 
 
 
--- tests/matrix_test.py
+++ tests/matrix_test.py
@@ -4,6 +4,13 @@ import unittest
 import random
 sys.path.append('../') # allows us to fetch files from the project root
 from modules.matrix import *
+from modules.open_digraph import *
+
+def print_matrix(matrix):
+  print("########################")
+  for i in range(len(matrix)):
+    print(matrix[i])
+  print("########################")
 
 class ListTest(unittest.TestCase):
   def test_random_int_list(self):
@@ -12,23 +19,81 @@ class ListTest(unittest.TestCase):
 
 class MatrixTest(unittest.TestCase):
   def test_random_int_matrix(self):
-    test = random_int_matrix(10,10)
+    test = random_matrix(10,10)
     for element in test:
       self.assertEqual(len(element),10)
 
   def test_random_int_matrix2(self):
-    test2 = random_int_matrix(10,10,null_diag=True)
+    test2 = random_matrix(10,10,null_diag=True)
     for i in range(0,10):
       self.assertEqual(test2[i][i],0)
 
   def test_sym_rand_matrix(self):
-    m = random_symetric_int_matrix(5, 10, null_diag = True)
+    m = random_matrix(5, 10, null_diag = True, symetric = True)
     for i in range(5):
       for j in range(5):
         self.assertEqual(m[i][j], m[j][i])
       self.assertEqual(m[i][i], 0)
+  
+  def test_oriented_int_matrix(self):
+    m = random_matrix(5, 10, oriented = True)
+    for i in range(5):
+      for j in range(5):
+        if m[i][j] != 0:
+          self.assertEqual(m[j][i], 0)
 
+  def test_triangular_int_matrix(self):
+    m = random_matrix(5, 10, triangular = True)
+    for i in range(5):
+      for j in range(i+1, 5):
+        self.assertEqual(m[i][j], 0)
+
+  def test_graph_from_adjacency_matrix(self):
+    self.assertEqual(graph_from_adjacency_matrix(random_matrix(3, 3)).is_well_formed(), True)
+    self.assertEqual(graph_from_adjacency_matrix(random_matrix(10, 9, null_diag=True)).is_well_formed(), True)
+    self.assertEqual(graph_from_adjacency_matrix(random_matrix(10, 9, symetric=True)).is_well_formed(), True)
+    self.assertEqual(graph_from_adjacency_matrix(random_matrix(10, 9, null_diag=True, symetric=True)).is_well_formed(), True)
+    self.assertEqual(graph_from_adjacency_matrix(random_matrix(10, 9, oriented=True)).is_well_formed(), True)
+    self.assertEqual(graph_from_adjacency_matrix(random_matrix(10, 9, triangular=True)).is_well_formed(), True)
+    self.assertEqual(graph_from_adjacency_matrix(random_matrix(10, 9, null_diag=True, triangular=True)).is_well_formed(), True)
+
+  def test_graph_from_adjacency_matrix(self):
+    self.assertEqual(random_graph(2, 2, inputs=0, outputs=0, form="free").is_well_formed(), True)
+    self.assertEqual(random_graph(2, 2, inputs=0, outputs=0, form="DAG").is_well_formed(), True)
+    self.assertEqual(random_graph(2, 2, inputs=0, outputs=0, form="oriented").is_well_formed(), True)
+    self.assertEqual(random_graph(2, 2, inputs=0, outputs=0, form="undirected").is_well_formed(), True)
+    self.assertEqual(random_graph(2, 2, inputs=0, outputs=0, form="loop-free undirected").is_well_formed(), True)
+
+'''
+  def test_print(self):
+    print("rand int matrix")
+    print_matrix(random_matrix(10, 9))
+    print("rand int matrix diag = 0")
+    print_matrix(random_matrix(10, 9, null_diag=True))
+    print("rand int matrix sym")
+    print_matrix(random_matrix(10, 9, symetric=True))
+    print("rand int matrix diag = 0 sym")
+    print_matrix(random_matrix(10, 9, null_diag=True, symetric=True))
+    print("rand int matrix oriented")
+    print_matrix(random_matrix(10, 9, oriented=True))
+    print("rand int matrix triangular")
+    print_matrix(random_matrix(10, 9, triangular=True))
+    print("rand int matrix diag = 0 triangular")
+    print_matrix(random_matrix(10, 9, null_diag=True, triangular=True))
+'''
 
 if __name__ == '__main__':  # the following code is called only when
   unittest.main()           # precisely this file is run.
+
+
+
+
+
+
+
+
+
+
+
+
 
